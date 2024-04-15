71548f97caccf24943c8070c64fe90536f844f3a
roAscurra <rominaascurra4887@gmail.com>
2024-04-15 18:41:33 -0300

    Entidad

---
+++ src/entities/Instrumento.ts
@@ -0,0 +1,11 @@
+export default class Instrumeno{
+    id:number = 0;
+    instrumento:string = "";
+    marca:string = "";
+    modelo:string = "";
+    imagen:string = "";
+    precio:number = 0;
+    costoEnvio:string = "G";
+    cantidadVendida:number = 0;
+    descripcion:string = "";
+}
