346b51088519ea9eb8f3f04b846d8329c9eaff56
roAscurra <rominaascurra4887@gmail.com>
2024-04-17 11:36:59 -0300

    Rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import Productos from "./components/Productos/Productos"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Productos from "./components/Productos/Productos";
+import DescripcionInstrumento from "./components/DescripcionInstrumento/DescripcionInstrumento";
 
 function App() {
-
   return (
-    <>
-    <Productos />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Productos />} />
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/descripcion-producto/:id" element={<DescripcionInstrumento />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
